0923ef132bcba6ee7f7b39d3630f29dc4199b17e
Feng Zheng <fzheng@codiscope.com>
2017-02-23 14:27:17 -0500

    updates

--- 2sgm/JumbleSort.java
+++ 2sgm/JumbleSort.java
@@ -1,45 +1,39 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Collections;
+import java.util.*;
 
 public class JumbleSort {
-    public static void main(String[] args) {
-        final String CHARSET_NAME = "UTF-8";
-        List<Integer> itemInteger = new ArrayList<Integer>();
-        List<String> itemString = new ArrayList<String>();
-        System.out.println("Input: ");
-        Scanner sc = new Scanner(System.in, CHARSET_NAME);
-        if (sc.hasNextLine()) {
-            String inputStr = sc.nextLine();
-            String[] items = inputStr.split("\\s+");
-            int itemNum = items.length;
-            int[] itemType = new int[itemNum];
-            for (int i = 0; i < itemNum; i++) {
-                if (items[i].matches("-?\\d+")) {
-                    itemInteger.add(Integer.parseInt(items[i]));
-                    itemType[i] = 1;
-                } else {
-                    itemString.add(items[i]);
-                    itemType[i] = 0;
-                }
-            }
-            Collections.sort(itemInteger);
-            Collections.sort(itemString);
-            System.out.println("Output: ");
-            Iterator<Integer> iteratorInt = itemInteger.iterator();
-            Iterator<String> iteratorStr = itemString.iterator();
-            for (int i = 0; i < itemNum; i++) {
-                if (itemType[i] == 1) {
-                    System.out.print(iteratorInt.next());
-                } else {
-                    System.out.print(iteratorStr.next());
-                }
-                System.out.print(" ");
-            }
-            System.out.println();
+  public static void main(String[] args) {
+    List<Integer> intList = new ArrayList<>();
+    List<String> strList = new ArrayList<>();
+    System.out.println("Input: ");
+    Scanner sc = new Scanner(System.in, "UTF-8");
+    if (sc.hasNextLine()) {
+      String[] items = sc.nextLine().split("\\s+");
+      int itemsLength = items.length;
+      boolean[] isNumber = new boolean[itemsLength];
+      for (int i = 0; i < itemsLength; i++) {
+        if (items[i].matches("-?\\d+")) {
+          intList.add(Integer.parseInt(items[i]));
+          isNumber[i] = true;
+        } else {
+          strList.add(items[i]);
+          isNumber[i] = false;
         }
-        sc.close();
+      }
+      Collections.sort(intList);
+      Collections.sort(strList);
+      System.out.println("Output: ");
+      Iterator<Integer> iteratorInt = intList.iterator();
+      Iterator<String> iteratorStr = strList.iterator();
+      for (int i = 0; i < itemsLength; i++) {
+        if (isNumber[i]) {
+          System.out.print(iteratorInt.next());
+        } else {
+          System.out.print(iteratorStr.next());
+        }
+        System.out.print(" ");
+      }
+      System.out.println();
     }
+    sc.close();
+  }
 }
